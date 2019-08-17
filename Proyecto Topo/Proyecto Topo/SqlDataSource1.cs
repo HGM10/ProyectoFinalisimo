502fced581f99fbe68d95993f1c36f112768b587
Heyner Hernandez <heyner717@outlook.es>
2019-08-17 13:11:14 -0600

    Mostrar busqueda en la tabla, aspecto diseno

---
+++ Proyecto Topo/Proyecto Topo/SqlDataSource1.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace Proyecto_Topo
+{
+    internal class SqlDataSource1
+    {
+        private SqlCommand cmd;
+
+        public SqlDataSource1(SqlCommand cmd)
+        {
+            this.cmd = cmd;
+        }
+    }
+}
