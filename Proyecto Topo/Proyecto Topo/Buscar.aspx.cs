b9664d01a58f60620b81e6bc226088b6a75bd265
Heyner Hernandez <heyner717@outlook.es>
2019-08-17 14:19:00 -0600

    Modificacion del modulo Buscar utilizando la base de datos creada por Kevin

--- Proyecto Topo/Proyecto Topo/Buscar.aspx.cs
+++ Proyecto Topo/Proyecto Topo/Buscar.aspx.cs
@@ -20,11 +20,11 @@ namespace Proyecto_Topo
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection("Data Source=.; Initial Catalog= reyno_fungi; Integrated Security= true");
-
+            SqlConnection con = new SqlConnection("Data Source=.; Initial Catalog= DBHongos_Nicaragua; Integrated Security= true");
+                   //select nombre as Nombre, forma as Forma, color as Color, tipo_clasificacion as Clasificacion from Hongo inner join clasificacion on fk_clasificacion=id_clasificacion where nombre= @nombre
             try
             {
-               SqlCommand cmd = new SqlCommand("select nombre as Nombre, forma as Forma, color as Color, tipo_clasificacion as Clasificacion from Hongo inner join clasificacion on fk_clasificacion=id_clasificacion where nombre= @nombre ", con);
+               SqlCommand cmd = new SqlCommand("select h.idHongo, c.nombre as Clase, o.nombre AS Orden, f.nombre AS Familia, g.nombre AS Género, h.especie as Nombre_Científico, cs.nombre AS Color_Sombrero, fs.nombre AS Forma_Sombrero, cp.nombre AS Color_Pie, fs.nombre AS Forma_Pie, h.descripcion, h.foto FROM Hongos h INNER JOIN Clase c ON h.id_clase = c.id_Clase INNER JOIN dbo.ColorPie cp ON h.id_color_pie = cp.id_Color_Pie INNER JOIN dbo.ColorSombrero cs ON h.id_color_sombrero = cs.id_Color_Sombrero INNER JOIN dbo.Familia f ON h.id_familia = f.id_Familia INNER JOIN dbo.FormaPie fp ON h.id_forma_pie = fp.id_Forma_Pie INNER JOIN dbo.FormaSombrero fs ON h.id_forma_sombrero = fs.id_Forma_Sombrero INNER JOIN dbo.Genero g ON h.id_genero = g.id_Genero INNER JOIN dbo.Orden o ON h.id_orden = o.id_Orden where h.especie like @nombre + '%' order by h.especie desc", con);
                cmd.Parameters.Add("@nombre", SqlDbType.VarChar).Value = TextBox1.Text;
                 con.Open();
                 SqlDataAdapter dr = new SqlDataAdapter(cmd);
--- Proyecto Topo/Proyecto Topo/guia.aspx
+++ Proyecto Topo/Proyecto Topo/guia.aspx
@@ -1,7 +1,9 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="guia.aspx.cs" Inherits="Proyecto_Topo.guia" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
 
 
